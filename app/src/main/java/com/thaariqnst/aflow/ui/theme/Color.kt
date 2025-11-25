d62116c998317caddbed38beb0d7ef1248459403
thaariqNasution <thaariqnst@gmail.com>
2025-11-26 06:13:45 +0700

    add more color

--- app/src/main/java/com/thaariqnst/aflow/ui/theme/Color.kt
+++ app/src/main/java/com/thaariqnst/aflow/ui/theme/Color.kt
@@ -7,6 +7,17 @@ val colorAccent = Color(0xFF2BEE8C)
 val bgWhite = Color(0xFFF6F8F7)
 val black = Color(0xFF111827)
 
+val blue300 = Color(0xFF82AAFF)
+
+val red300 = Color(0xFFFF8282)
+
+val yellow300 = Color(0xFFFFC882)
+
+val green300 = Color(0xFF82FFAA)
+
+val cyan300 = Color(0xFF82DFFF)
+
+val purple300 = Color(0xFFD282FF)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
