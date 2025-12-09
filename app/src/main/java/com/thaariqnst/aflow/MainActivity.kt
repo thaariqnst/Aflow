a1d8509788d78149e82438b8e28418e4f3b38013
thaariqNasution <thaariqnst@gmail.com>
2025-12-09 22:33:59 +0700

    use NavController to navigate between screens

--- app/src/main/java/com/thaariqnst/aflow/MainActivity.kt
+++ app/src/main/java/com/thaariqnst/aflow/MainActivity.kt
@@ -5,9 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.thaariqnst.aflow.ui.home.HomeScreen
 import com.thaariqnst.aflow.ui.onboarding.OnboardingScreen
 import com.thaariqnst.aflow.ui.theme.AflowTheme
 
@@ -18,11 +22,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             AflowTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    OnboardingScreen {
-
-                    }
+                    App()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "onboarding"
+    ) {
+        composable("onboarding") { OnboardingScreen { navController.navigate("home") } }
+        composable("home") { HomeScreen() }
     }
 }
